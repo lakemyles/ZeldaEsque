7e27612b19cacebfea19460029b427f5752a4970
Myles Lake <lakemyles@gmail.com>
2021-01-01 13:39:48 -0600

    Add four directional player animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,14 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,9 +24,21 @@ public class PlayerMovement : MonoBehaviour
         change.x = Input.GetAxisRaw("Horizontal");  // GetAxis has an accelation component rather than going straight to player speed
         change.y = Input.GetAxisRaw("Vertical");    // GetAxisRaw goes straight to the player speed set
         //Debug.Log(change);                        // Used debug to show the speed in the bottom left corner while playing
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
         if (change != Vector3.zero)
         {
             MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
         }
     }
 
