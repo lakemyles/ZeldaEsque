73b8d49ece056f068e422f2666de4cf959f83f1a
Myles Lake <lakemyles@gmail.com>
2021-01-02 12:21:48 -0600

    Add smooth camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // LateUpdate comes after Update so player moves on Update 
+    // and camera moves on LateUpdate
+    void LateUpdate()
+    {
+        if(transform.position != target.position)
+        {
+            Vector3 targetPostition = new Vector3(target.position.x, 
+                target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position,
+                targetPostition, smoothing);
+        }
+    }
+}
