08786ecf3bf15dcffaf025abc232240491024614
Myles Lake <lakemyles@gmail.com>
2021-01-02 13:16:18 -0600

    Add camera bounds git status

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,6 +6,8 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform target;
     public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +23,10 @@ public class CameraMovement : MonoBehaviour
         {
             Vector3 targetPostition = new Vector3(target.position.x, 
                 target.position.y, transform.position.z);
+            targetPostition.x = Mathf.Clamp(targetPostition.x,
+                minPosition.x, maxPosition.x);
+            targetPostition.y = Mathf.Clamp(targetPostition.y,
+                minPosition.y, maxPosition.y);
             transform.position = Vector3.Lerp(transform.position,
                 targetPostition, smoothing);
         }
